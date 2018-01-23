b0258852816a93c29fb430d7c9e7424b461db58c
Dino Trojak <dinotrojak@gmail.com>
2018-01-23 16:39:25 +0100

    added footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import './footer.css'
+
+const Footer = () => (
+    <div className="footer">
+        <h3>Ovo je footer</h3>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis ea, libero id sint rem, beatae quia inventore ullam perferendis sed sequi aspernatur nemo aliquid, fuga hic necessitatibus! Assumenda, quasi facilis.</p>
+    </div>
+)
+
+export default Footer
